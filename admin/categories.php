3158b913c9efd343e582b67fe0479836b2e37b45
schonhose <schonhose@ff196f25-2e1a-0410-95ca-3d8fd8d346c5>
2006-11-13 20:37:24 +0000

    Added bilingual support to categories. Will not be displayed when addon is disabled.
    git-svn-id: http://pixelpost.googlecode.com/svn/trunk@31 ff196f25-2e1a-0410-95ca-3d8fd8d346c5

--- admin/categories.php
+++ admin/categories.php
@@ -11,6 +11,11 @@ if(!isset($_SESSION["pixelpost_admin"]) || $cfgrow['password'] != $_SESSION["pix
 
 // categories
 if($_GET['view'] == "categories") {
+// added for language support
+// Check if the language addon is enabled. If not there is no need to show these fields
+$language_enabled = sql_array("SELECT * FROM ".$pixelpost_db_prefix."addons where addon_name = 'admin_language'");
+$language_status = pullout($language_enabled['status']);
+
 echo "<div id='caption'>$admin_lang_categories</div>";
 
     if($_GET['action'] == "delete") {
@@ -19,27 +24,43 @@ echo "<div id='caption'>$admin_lang_categories</div>";
         }
     if($_GET['action'] == "add") {
         $category = clean($_POST['category']);
-		    $query = sql_query("insert into ".$pixelpost_db_prefix."categories(id,name) VALUES(NULL,'$category')");
+        if (isset($_POST['alt_category'])) {
+ 		 		 //Obviously we would like to add secondary language
+					$alt_category = clean($_POST['alt_category']);
+					} else {
+						$alt_category = "";
+				}
+		    $query = sql_query("insert into ".$pixelpost_db_prefix."categories(id,name,alt_name) VALUES(NULL,'$category','$alt_category')");
   		   echo "<div class='content confirm'>$admin_lang_done $admin_lang_cats_added<p /></div>
 	    ";
         }
     if($_GET['action'] == "edit") {
 	$query = sql_array("select * from ".$pixelpost_db_prefix."categories where id='".$_POST['id']."'");
         $name = pullout($query['name']);
+        $alt_name = pullout($query['alt_name']);
         echo "<div class='jcaption'>$admin_lang_cats_edit_cat_txt</div>
         <div class='content'>$admin_lang_cats_edit_tip<p />
 
 	<form method='post' action='$PHP_SELF?view=categories&amp;action=update&amp;id=".$query['id']."' accept-charset='UTF-8'>
-        <input type='text' name='category' value='$name' style='width:300px;'/>
-        <input type='submit' value='$admin_lang_cats_update_cat_button' />
+        <input type='text' name='category' value='$name' style='width:300px;'/>";
+     		if ($language_status =='on'){
+	        echo "<br /><input type='text' name='alt_category' value='$alt_name' style='width:300px;'/>";
+	      }
+        echo "<input type='submit' value='$admin_lang_cats_update_cat_button' />
         </form>
         </div>
         ";
         }
     if($_GET['action'] == "update") {
         $category = clean($_POST['category']);
+        if (isset($_POST['alt_category'])) {
+ 		 		 //Obviously we would like to add secondary language
+					$alt_category = clean($_POST['alt_category']);
+					} else {
+						$alt_category = "";
+				}
         $getid = $_GET['id'];
-        $upquery = sql_query("update ".$pixelpost_db_prefix."categories set name='$category' where id='$getid'");
+        $upquery = sql_query("update ".$pixelpost_db_prefix."categories set name='$category', alt_name='$alt_category' where id='$getid'");
         // echo "<div class='jcaption'>$admin_lang_update</div>
         echo "<div class='content confirm'>$admin_lang_done $admin_lang_cats_updated<p /></div>";
         }
@@ -47,8 +68,12 @@ echo "<div id='caption'>$admin_lang_categories</div>";
     <div class='jcaption'>$admin_lang_cats_add_cat</div>
     <div class='content'>$admin_lang_cats_add_cat_txt<p />
     <form method='post' action='$PHP_SELF?view=categories&amp;action=add' accept-charset='UTF-8'>
-    <input type='text' name='category' style='width:300px;' /><p />
-    <input type='submit' value='$admin_lang_cats_add_cat' />
+    <input type='text' name='category' style='width:300px;' /><p />";
+		if ($language_status =='on'){
+    	echo "Secondary language:<br />
+    				<input type='text' name='alt_category' style='width:300px;' /><p />";
+    }
+    echo "<input type='submit' value='$admin_lang_cats_add_cat' />
     </form>
     </div>
     <div class='jcaption'>$admin_lang_cats_edit_cat</div>
@@ -59,10 +84,15 @@ echo "<div id='caption'>$admin_lang_categories</div>";
     <option value=''>----------</option>
     ";
     $query = mysql_query("select * from ".$pixelpost_db_prefix."categories order by name");
-    while(list($id,$name) = mysql_fetch_row($query)) {
+    while(list($id,$name,$alt_name) = mysql_fetch_row($query)) {
         $name = pullout($name);
-        echo "<option value='$id'>$name</option>\n";
-        }
+        if ($language_status =='on'){
+    			$alt_name = pullout($alt_name);
+    			echo "<option value='$id'>".$name." (".$alt_name.")</option>\n";
+    		}else {
+        	echo "<option value='$id'>$name</option>\n";
+      	}
+       }
     echo "
     </select><p />
     <input type='submit' value='$admin_lang_cats_edit_cat_button' />
@@ -77,12 +107,15 @@ echo "<div id='caption'>$admin_lang_categories</div>";
     <option value=''>----------</option>
     ";
     $query = mysql_query("select * from ".$pixelpost_db_prefix."categories order by name");
-    while(list($id,$name) = mysql_fetch_row($query)) {
-    	if ($id!=1){
-			$name = pullout($name);
-			echo "<option value='$id'>$name</option>\n";
-        	}
-        }
+    while(list($id,$name,$alt_name) = mysql_fetch_row($query)) {
+        $name = pullout($name);
+        if ($language_status =='on'){
+    			$alt_name = pullout($alt_name);
+    			echo "<option value='$id'>".$name." (".$alt_name.")</option>\n";
+    		}else {
+        	echo "<option value='$id'>$name</option>\n";
+      	}
+       }
     echo "
     </select><p />
     <input type='submit' value='$admin_lang_cats_delete_cats_button' />
