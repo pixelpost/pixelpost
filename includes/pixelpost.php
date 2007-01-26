ed437826eb73866f5a387c77e4480c15ebcbbfdb
piotr.galas <piotr.galas@ff196f25-2e1a-0410-95ca-3d8fd8d346c5>
2007-01-26 00:21:57 +0000

    please dont change it!! create at your local boxes correct DB with user and its pass which should be the same as here is
    git-svn-id: http://pixelpost.googlecode.com/svn/trunk@118 ff196f25-2e1a-0410-95ca-3d8fd8d346c5

--- includes/pixelpost.php
+++ includes/pixelpost.php
@@ -38,10 +38,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // database variables - this is info you've received from you hosting service
 // those are needed for the script to connect and use MySQL
 
-$pixelpost_db_host      = "localhost"; // database host, often "localhost"
-$pixelpost_db_user      = "root"; // database user
-$pixelpost_db_pass      = "dennis"; // database user password
-$pixelpost_db_pixelpost = "pixelpost"; // database
+$pixelpost_db_host      = "replace_me"; // database host, often "localhost"
+$pixelpost_db_user      = "replace_me"; // database user
+$pixelpost_db_pass      = "replace_me"; // database user password
+$pixelpost_db_pixelpost = "replace_me"; // database
 
 $pixelpost_db_prefix = "pixelpost_"; // table prefix, leave as is unless you want to install multiple blogs on the same database
 
