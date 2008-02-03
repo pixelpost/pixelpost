6451939fb71b97dee32e3879d230d4b27d7e1698
schonhose <schonhose@ff196f25-2e1a-0410-95ca-3d8fd8d346c5>
2008-02-03 11:30:52 +0000

    Added more logic in the upload process. The old way showed the entire empty form with the thumbnail of the uploaded file at the bottom after upload. I restructured it so now you will only see the thumbnail creation instead of the whole form. So now the upload is a two page process: 1) the form, 2) the thumbnail creation.
    git-svn-id: http://pixelpost.googlecode.com/svn/trunk@560 ff196f25-2e1a-0410-95ca-3d8fd8d346c5

--- admin/new_image.php
+++ admin/new_image.php
@@ -13,8 +13,7 @@ if(!isset($_SESSION["pixelpost_admin"]) || $cfgrow['password'] != $_SESSION["pix
 // if no page is specified return a new post / image upload thing
 if(!isset($_GET['view']) OR $_GET['view'] == '')
 {
-	$show_image_after_upload = True; // For default behavior this is set to 'True' you can change this to false in your addons in the new image page
-
+	$show_thumbnail_creation_page = false; // For default behavior this is set to 'False' you can change this to True in your addons in the new image page
    // save new post
 	if(isset($_GET['x']) AND $_GET['x'] == "save")
 	{
@@ -87,7 +86,7 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 			$uploadfile = $upload_dir .$time_stamp_r .$userfile;
 
 			// NEW WORKSPACE ADDED
-      eval_addon_admin_workspace_menu('image_upload_start');
+      		eval_addon_admin_workspace_menu('image_upload_start');
 
 			if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
 			{
@@ -118,9 +117,9 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 				    $datetime = date("Y-m-d H:i:s", mktime($exifhour, $exifmin, $exifsec, $exifmonth, $exifday, $exifyear));
 				  }
 				  else	$datetime = gmdate("Y-m-d H:i:s",time()+(3600 * $tz));
-	      }
-	      // NEW WORKSPACE ADDED
-        eval_addon_admin_workspace_menu('image_upload_succesful');
+	      		}
+	      		// NEW WORKSPACE ADDED
+        		eval_addon_admin_workspace_menu('image_upload_succesful');
 			}
 			else
 			{
@@ -136,7 +135,7 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 				echo "</div><hr/>";
 
 	 			// NEW WORKSPACE ADDED
-        eval_addon_admin_workspace_menu('image_upload_failed');
+        		eval_addon_admin_workspace_menu('image_upload_failed');
 			} // end move
 		} // end prepare of file ($_FILES['userfile'] != "")
 
@@ -149,7 +148,7 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 			VALUES('$datetime','$headline','$body','$image','$alt_headline','$alt_body','$comments_settings','$exif_info_db')";
 			$result = mysql_query($query) || die("Error: ".mysql_error().$admin_lang_ni_db_error);
 
-	    $theid = mysql_insert_id(); //Gets the id of the last added image to use in the next "insert"
+	    	$theid = mysql_insert_id(); //Gets the id of the last added image to use in the next "insert"
 
 			if(isset($_POST['category']))
 			{
@@ -163,12 +162,66 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 	
 				$query_st = "INSERT INTO ".$pixelpost_db_prefix."catassoc (id,cat_id,image_id) VALUES ".implode(",", $query_val).";";
 				$result = mysql_query($query_st) || die("Error: ".mysql_error());
-	    }
-	    // done
-
+	    	}
+	    	// done
+	    	// moved by Dennis to get a more consistent upload process.
+			if ($show_thumbnail_creation_page){
+				$to_echo = "<div id='caption'>$admin_lang_ni_posted: $headline</div>
+		 			<div class='content'>$body<br/>
+		 			$datetime<br/><a href=\"".PHP_SELF."?view=images&id=$theid\">[$admin_lang_imgedit_edit]</a><p>";
+		 	} else {
+				if ($cfgrow['crop'] != '12c')
+				{
+					$host  = $_SERVER['HTTP_HOST'];
+					$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+					$extra = 'index.php?view=images';
+					echo '<meta http-equiv="refresh" content="0;url=http://'.$host.$uri."/".$extra.'" />';
+				}	
+			}
+			// Check if the '12c' is selected as the crop then add 3 buttons to the page '+', '-', and 'crop'
+			if($cfgrow['crop']=='12c')
+			{
+				$to_echo .="$admin_lang_ni_crop_nextstep<br/>
+						 <input type='button' name='Submit1' value='".$txt['cropimage']."' onclick=\"cropCheck('def','".$filnamn ."');\"/>
+						 <input type='button' name='Submit3' value='".$txt['smaller']."' onmousedown=\"cropZoom('in');\" onmouseup='stopZoom();'/>
+						 <input type='button' name='Submit4' value='".$txt['bigger']."' onmousedown=\"cropZoom('out');\" onmouseup='stopZoom();'/>
+						 <br/> ";
+			}
+			echo $to_echo; // tag of content div still open
+			//create thumbnail
+			if(function_exists('gd_info'))
+			{
+				$gd_info = gd_info();
+				if($gd_info != "")
+				{
+					$thumbnail = $filnamn;
+					$thumbnail = createthumbnail($thumbnail);
+					eval_addon_admin_workspace_menu('thumb_created');
+					// if crop is not '12c' use the oldfashioned crop
+					if($cfgrow['crop']!='12c')
+					{
+						if($show_image_after_upload)	echo "<img src='".$cfgrow['imagepath'].$filnamn."'/>";
+						echo "</div><!-- end of content div -->" ; // close content div
+					}
+					else
+					{
+						// set the size of the crop frame according to the uploaded image
+						setsize_cropdiv ($filnamn);
+						$for_echo ="<img src='".$cfgrow['imagepath'].$filnamn."' id='myimg'/>
+							<div id='cropdiv'>
+							<table width='100%' height='100%' border='1' cellpadding='0' cellspacing='0' bordercolor='#000000'>
+							<tr><td><img src='".$spacer."'/></td></tr>
+							</table></div> <!-- end of crop div -->
+							<div id='editthumbnail'>
+							<hidden>$admin_lang_ni_crop_background</hidden>
+							</div><!-- end of editthumbnail id -->
+							</div> <!-- end of content div -->  ";
+						echo $for_echo;
+					} // end else
+				} // gd info
+			} // function_exists
 			// workspace: image_uploaded
 			eval_addon_admin_workspace_menu('image_uploaded');
-
 			// save tags
 			save_tags_new(clean($_POST['tags']),$theid);
 
@@ -185,6 +238,8 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 	{
 		unset($alt_headline, $alt_tags, $alt_body, $_POST['category'], $_POST['autodate'], $_POST['post_year'], $_POST['post_month'], $_POST['post_day'], $_POST['post_hour'], $_POST['post_minute'], $_POST['allow_comments']);
 	}
+	if ($_GET['x'] != "save")
+    {
 ?>
 
 	<form method="post" action="<?php echo PHP_SELF;?>?x=save" enctype="multipart/form-data" accept-charset="UTF-8">
@@ -198,9 +253,9 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 	<?php echo $admin_lang_ni_tags;?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
    <input type="text" name="tags" style="width:550px;" value="<?php if(isset($status) && $status!='ok') echo $_POST['tags'];?>"/><br/>
    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<?php echo $admin_lang_ni_tags_desc;?><p/>
-  <?php	eval_addon_admin_workspace_menu('new_image_form_def_lang'); 	?>
-  <?php echo $admin_lang_ni_select_cat;?>
-	<?php
+  	<?php	
+	eval_addon_admin_workspace_menu('new_image_form_def_lang'); 
+  	echo $admin_lang_ni_select_cat;
 	category_list_as_table(clean(isset($_POST['category'])), $cfgrow);
  	$tz = $cfgrow["timezone"];
  	$cur_time = gmdate("Y-m-d H:i:s",time()+(3600 * $tz));
@@ -403,77 +458,6 @@ if(!isset($_GET['view']) OR $_GET['view'] == '')
 	</form>
 
 <?php
-	if(isset($_GET['x']) AND $_GET['x'] == "save" AND $status == "ok")
-	{
-		$headline = pullout($_POST['headline']);
-		$body = pullout($_POST['body']);
-		$headline = htmlspecialchars($headline,ENT_QUOTES);
-		$body = htmlspecialchars($body,ENT_QUOTES);
-		$to_echo = "
-		 <div id='caption'>$admin_lang_ni_posted: $headline</div>
-		 <div class='content'>$body<br/>
-		 $datetime<br/><a href=\"".PHP_SELF."?view=images&id=$theid\">[$admin_lang_imgedit_edit]</a><p>
-		 ";
-
-		// Check if the '12c' is selected as the crop then add 3 buttons to the page '+', '-', and 'crop'
-		if($cfgrow['crop']=='12c')
-		{
-			$to_echo .="
-						 $admin_lang_ni_crop_nextstep<br/>
-						 <input type='button' name='Submit1' value='".$txt['cropimage']."' onclick=\"cropCheck('def','".$filnamn ."');\"/>
-						 <input type='button' name='Submit3' value='".$txt['smaller']."' onmousedown=\"cropZoom('in');\" onmouseup='stopZoom();'/>
-						 <input type='button' name='Submit4' value='".$txt['bigger']."' onmousedown=\"cropZoom('out');\" onmouseup='stopZoom();'/>
-						 <br/> ";
-		};
-
-		echo $to_echo; // tag of content div still open
-
-		//create thumbnail
-		if(function_exists('gd_info'))
-		{
-			$gd_info = gd_info();
-
-			if($gd_info != "")
-			{
-				$thumbnail = $filnamn;
-				$thumbnail = createthumbnail($thumbnail);
-
-				eval_addon_admin_workspace_menu('thumb_created');
-
-				// if crop is not '12c' use the oldfashioned crop
-				if($cfgrow['crop']!='12c')
-				{
-					if($show_image_after_upload)	echo "<img src='".$cfgrow['imagepath'].$filnamn."'/>";
-
-					echo "</div><!-- end of content div -->" ; // close content div
-				}// end if
-				/* else it is '12c' crop and show cropdiv and the cropping frame
-						at the bottom of the page.
-				*/
-				else
-				{
-				// set the size of the crop frame according to the uploaded image
-					setsize_cropdiv ($filnamn);
-				//--------------------------------------------------------
-					$for_echo ="
-						<img src='".$cfgrow['imagepath'].$filnamn."' id='myimg'/>
-						<div id='cropdiv'>
-						<table width='100%' height='100%' border='1' cellpadding='0' cellspacing='0' bordercolor='#000000'>
-						<tr>
-						<td><img src='".$spacer."'/></td>
-						</tr>
-						</table>
-						</div> <!-- end of crop div -->
-						<div id='editthumbnail'>
-						<hidden>$admin_lang_ni_crop_background</hidden>
-						</div><!-- end of editthumbnail id -->
-
-					</div> <!-- end of content div -->  ";
-					echo $for_echo;
-				//--------------------------------------------------------
-				} // end else
-			} // gd info
-		} // function_exists
 	}
 } // end view=null
 ?>
